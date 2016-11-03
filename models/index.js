3e489b79f585e210ca7eb1325b6c09c2bed539c0
14budig <nbudig@gmail.com>
2016-11-03 09:46:17 -0700

    literally no change

--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 var mongoose = require("mongoose");
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/twitch-up");
 
+
 module.exports.Event = require('./event.js');
 module.exports.User = require('./user.js');
